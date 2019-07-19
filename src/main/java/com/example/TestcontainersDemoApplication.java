0398d00d399c57a9354df53c074f5cd516e7aac6
Zoltan Altfatter <altfatterz@gmail.com>
2019-07-19 17:52:07 +0200

    working example with reactive usecase

--- src/main/java/com/example/TestcontainersDemoApplication.java
+++ src/main/java/com/example/TestcontainersDemoApplication.java
@@ -6,15 +6,18 @@ import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.ReactiveMongoDatabaseFactory;
+import org.springframework.data.mongodb.ReactiveMongoTransactionManager;
 import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.ReactiveTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
+import org.springframework.util.Assert;
+import org.springframework.util.StringUtils;
+import reactor.core.publisher.Flux;
 
 @SpringBootApplication
 @EnableTransactionManagement
@@ -24,6 +27,10 @@ public class TestcontainersDemoApplication {
         SpringApplication.run(TestcontainersDemoApplication.class, args);
     }
 
+    @Bean
+    ReactiveTransactionManager reactiveTransactionManager(ReactiveMongoDatabaseFactory rmdf) {
+        return new ReactiveMongoTransactionManager(rmdf);
+    }
 }
 
 @Service
@@ -32,17 +39,20 @@ class CustomerService {
 
     private final CustomerRepository customerRepository;
 
+
     @Transactional
-    void saveAll(String... names) {
-        List<Customer> customers = Arrays.stream(names)
+    public Flux<Customer> saveAll(String... names) {
+        Flux<Customer> customers = Flux.just(names)
                 .map(name -> new Customer(null, name))
-                .collect(Collectors.toList());
-        customerRepository.saveAll(customers);
+                .flatMap(customerRepository::save)
+                .doOnNext(customer -> Assert.isTrue(!StringUtils.isEmpty(customer.getName()), "'names' query parameter must not be empty"));
+
+        return customers;
     }
 
 }
 
-interface CustomerRepository extends CrudRepository<Customer, String> {
+interface CustomerRepository extends ReactiveCrudRepository<Customer, String> {
 }
 
 @Data
--- src/test/java/com/example/CustomerServiceTest.java
+++ src/test/java/com/example/CustomerServiceTest.java
@@ -1,6 +1,6 @@
 package com.example;
 
-import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.ClassRule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -12,6 +12,7 @@ import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.testcontainers.containers.GenericContainer;
+import reactor.test.StepVerifier;
 
 import java.io.IOException;
 
@@ -28,7 +29,14 @@ public class CustomerServiceTest {
 
     @ClassRule
     public static GenericContainer mongo = new GenericContainer<>("mongo:4.0.10")
-            .withExposedPorts(27017);
+            .withExposedPorts(27017).withCommand("--replSet tx-replica-set");
+
+    @BeforeClass
+    public static void setup() throws IOException, InterruptedException {
+        mongo.execInContainer("/bin/bash", "-c", "mongo test --eval 'rs.initiate()'");
+        mongo.execInContainer("/bin/bash", "-c", "until mongo --eval \"printjson(rs.isMaster())\" | grep ismaster | grep true > /dev/null 2>&1;do sleep 1;done");
+        mongo.execInContainer("/bin/bash", "-c", "mongo test --eval 'db.createCollection(\"customers\")'");
+    }
 
     static class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
         public void initialize(ConfigurableApplicationContext configurableApplicationContext) {
@@ -38,15 +46,28 @@ public class CustomerServiceTest {
         }
     }
 
-    @Before
-    public void setUp() throws IOException, InterruptedException {
-        customerRepository.deleteAll();
+    @Test
+    public void saveAllHappyFlow() {
+        StepVerifier.create(this.customerRepository.deleteAll())
+                .verifyComplete();
+
+        StepVerifier.create(this.customerService.saveAll("John Doe", "Jane Doe"))
+                .expectNextCount(2).verifyComplete();
+
+        StepVerifier.create(this.customerRepository.findAll())
+                .expectNextCount(2).verifyComplete();
     }
 
     @Test
-    public void saveAll() {
-        customerService.saveAll("John Doe", "Jane Doe");
-        customerRepository.findAll().forEach(System.out::println);
+    public void saveAllWithRollback() {
+        StepVerifier.create(this.customerRepository.deleteAll())
+                .verifyComplete();
+
+        StepVerifier.create(this.customerService.saveAll("John Doe", "Jane Doe", "", "Mike Doe"))
+                .expectNextCount(1).expectError().verify();
+
+        StepVerifier.create(this.customerRepository.findAll())
+                .expectNextCount(0).verifyComplete();
     }
 
 
